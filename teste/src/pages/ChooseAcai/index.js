3e5dd7b302cad4cf3b036353c1d4f82b5435f6a1
talita-aya <tatakuriki@gmail.com>
2021-11-04 15:27:40 -0300

    first page model

--- teste/src/pages/ChooseAcai/index.js
+++ teste/src/pages/ChooseAcai/index.js
@@ -4,9 +4,34 @@ import './style.css';
 
 const index = () => {
   return (
-    <div>
-      eai
-    </div>
+    <>
+      <div className="container">
+        <div className="content">
+          <h1>Escolha seu açaí</h1>
+
+          <div className="options">
+            <p>SABOR:</p>
+            <div className="button">
+              <button>Morango</button>
+              <button>Banana</button>
+              <button>Kiwi</button>
+            </div>
+
+            <p>TAMANHO:</p>
+            <div className="button">
+              <button>Pequeno (300ml)</button>
+              <button>Médio (500ml)</button>
+              <button>Grande (700ml)</button>
+            </div>
+
+            <div className="continue">
+              <button>Avançar</button>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
